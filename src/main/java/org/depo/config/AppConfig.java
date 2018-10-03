6a28cf2728be99aea89c95e75026910866a5f73e
Denis Ponizovkin <denis.ponizovkin@gmail.com>
2018-10-03 18:25:20 +0300

    just parse

--- src/main/java/org/depo/config/AppConfig.java
+++ src/main/java/org/depo/config/AppConfig.java
@@ -1,14 +1,8 @@
 package org.depo.config;
 
 import org.apache.log4j.Logger;
-import org.depo.job.SaveJob;
 import org.depo.model.EmployeeParser;
 import org.depo.service.EmployeeService;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.impl.JobDetailImpl;
-import org.quartz.impl.StdSchedulerFactory;
-import org.quartz.impl.triggers.CronTriggerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,24 +20,10 @@ public class AppConfig {
     EmployeeService employeeService;
 
     @PostConstruct
-    public void init() throws ParseException, SchedulerException {
-        ep.parse(employeeService);
-
+    public void init() throws ParseException {
         try {
-            JobDetailImpl job = new JobDetailImpl();
-            job.setName("Employee parser");
-            job.setJobClass(SaveJob.class);
-
-            CronTriggerImpl trigger = new CronTriggerImpl();
-            trigger.setName("cronTrigger");
-            trigger.setCronExpression("0 * * * * ?");
-
-            //Execute the job.
-            Scheduler scheduler =
-                    new StdSchedulerFactory().getScheduler();
-            scheduler.start();
-            scheduler.scheduleJob(job, trigger);
-        } catch (Exception e) {
+            ep.parse(employeeService);
+      } catch (Exception e) {
             LOGGER.error(e.getMessage());
         }
     }
